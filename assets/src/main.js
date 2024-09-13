f4b7f79945bb4707084c31b53e6d649efde5df19
L1nk01 <ecampospaulino@gmail.com>
2024-09-13 02:27:44 -0400

    extension design finished

--- assets/src/main.js
+++
@@ -1,8 +0,0 @@
-console.log("The extension is up and running");
-
-var images = document.getElementsByTagName('img');
-
-// for (elt of images){
-//    elt.src = `${browser.runtime.getURL("pp.jpg")}`;
-//    elt.alt = 'an alt text';
-// }
--- assets/src/popup.js
+++ assets/src/popup.js
@@ -1,11 +1,17 @@
 const getUrlBtn = document.querySelector("#get-url-btn");
 const urlInput = document.querySelector("#url-input");
+const cells = document.querySelectorAll('#playlist-info tr td:nth-child(2)');
 
 getUrlBtn.addEventListener("click", () => {
     browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
         const activeTab = tabs[0];
         const tabUrl = activeTab.url;
         urlInput.value = tabUrl;
-        // Puedes hacer algo más con la URL, como enviarla al popup o almacenarla.
       });
 });
+
+cells[0].innerText = "2h 35m 40s";
+cells[1].innerText = "5m 23s";
+cells[2].innerText = 28;
+cells[3].innerText = "1m 50s";
+cells[4].innerText = "15m 32s";
