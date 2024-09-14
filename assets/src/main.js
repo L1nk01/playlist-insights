80c984a870a63f8a1fafea101bf5056b5cfb919f
L1nk01 <ecampospaulino@gmail.com>
2024-09-14 00:58:02 -0400

    Add new logic to handle YouTube playlist and video URLs in main.js

---
+++ assets/src/main.js
@@ -0,0 +1,38 @@
+function getVideoTimeElements(url) {
+  const urlType = checkUrlType(url);
+
+  if (urlType === 1) { // Playlist
+    return document.querySelectorAll("div#content.style-scope.ytd-playlist-video-renderer #container ytd-thumbnail #thumbnail #overlays ytd-thumbnail-overlay-time-status-renderer #time-status #text");
+  } else if (urlType === 2) { // Watching a playlist video
+    return document.querySelectorAll('ytd-playlist-panel-video-renderer #overlays ytd-thumbnail-overlay-time-status-renderer .badge-shape-wiz__text');
+  } else {
+    return [];
+  }
+}
+
+function checkUrlType(url) {
+  let path = new URL(url).pathname;
+  let params = new URL(url).searchParams;
+
+  if (path.startsWith('/playlist')) {
+    return 1; // Playlist URL
+  } else if (path.startsWith('/watch') && params.has('list')) {
+    return 2; // Playlist video URL
+  } else {
+    return 0; // Neither playlist nor playlist video
+  }
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "getVideoTimeElements") {
+    let url = request.url;
+
+    let videoTimeElements = getVideoTimeElements(url);
+    videoTimeElements = Array.from(videoTimeElements).map(element => element.innerText.trim());
+
+    sendResponse({
+      message: "URL processed",
+      videoTimeElements: videoTimeElements
+    });
+  }
+});
