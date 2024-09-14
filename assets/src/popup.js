356cb965d7cce5dda83f84dcff78df7db8528846
L1nk01 <ecampospaulino@gmail.com>
2024-09-14 00:58:38 -0400

    implemented logic to get playlist data

--- assets/src/popup.js
+++ assets/src/popup.js
@@ -1,17 +1,146 @@
-const getUrlBtn = document.querySelector("#get-url-btn");
+const buttons = {
+  getUrlBtn: document.querySelector("#get-url-btn"),
+  getInfoBtn: document.querySelector("#get-info-btn"),
+  resetDataBtn: document.querySelector("#reset-data-btn")
+}
 const urlInput = document.querySelector("#url-input");
-const cells = document.querySelectorAll('#playlist-info tr td:nth-child(2)');
+const tableDataCells = document.querySelectorAll('#playlist-info tr td:nth-child(2)');
 
-getUrlBtn.addEventListener("click", () => {
-    browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-        const activeTab = tabs[0];
-        const tabUrl = activeTab.url;
-        urlInput.value = tabUrl;
-      });
+buttons.getUrlBtn.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+    let activeTab = tabs[0];
+    let tabUrl = activeTab.url;
+    urlInput.value = tabUrl;
+  });
 });
 
-cells[0].innerText = "2h 35m 40s";
-cells[1].innerText = "5m 23s";
-cells[2].innerText = 28;
-cells[3].innerText = "1m 50s";
-cells[4].innerText = "15m 32s";
+buttons.getInfoBtn.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    let urlValue = urlInput.value;
+    let activeTabId = tabs[0].id;
+
+    chrome.tabs.sendMessage(activeTabId, { action: "getVideoTimeElements", url: urlValue }, (response) => {
+      if (chrome.runtime.lastError) {
+        alert("URL IS NOT A VALID YOUTUBE PLAYLIST URL");
+        return;
+      }
+
+      if (response && response.videoTimeElements) {
+        let videoTimeElements = response.videoTimeElements;
+
+        updateTable(getPlaylistData(videoTimeElements));
+      }
+    });
+  });
+});
+
+buttons.resetDataBtn.addEventListener("click", (resetData));
+
+function updateTable(videoInfo) {
+  tableDataCells[0].innerText = `${videoInfo.playlistDuration}`;
+  tableDataCells[1].innerText = `${videoInfo.playlistAverageDuration}`;
+  tableDataCells[2].innerText = `${videoInfo.playlistTotalVideos}`;
+  tableDataCells[3].innerText = `${videoInfo.playlistShortestVideo}`;
+  tableDataCells[4].innerText = `${videoInfo.playlistLongestVideo}`;
+}
+
+function resetData() {
+  urlInput.value = "";
+  tableDataCells[0].innerText = "";
+  tableDataCells[1].innerText = "";
+  tableDataCells[2].innerText = "";
+  tableDataCells[3].innerText = "";
+  tableDataCells[4].innerText = "";
+}
+
+function getPlaylistData(videoTimeElements) {
+  let data = {
+    playlistDuration: playlist.getPlaylistDuration(videoTimeElements),
+    playlistAverageDuration: playlist.getPlaylistAverageDuration(videoTimeElements),
+    playlistTotalVideos: playlist.getPlaylistTotalVideos(videoTimeElements),
+    playlistShortestVideo: playlist.getPlaylistShortestVideo(videoTimeElements),
+    playlistLongestVideo: playlist.getPlaylistLongestVideo(videoTimeElements)
+  }
+
+  return data;
+}
+
+function convertTimeToSeconds(time) {
+  let [minutes, seconds] = time.split(":").map(Number);
+
+  if (isNaN(minutes) || isNaN(seconds)) {
+    return 0;
+  }
+
+  return (minutes * 60) + seconds;
+}
+
+function convertSecondsToTime(durationSeconds) {
+  const time = {
+    hours: Math.floor(durationSeconds / 3600),
+    minutes: Math.floor((durationSeconds % 3600) / 60),
+    seconds: durationSeconds % 60
+  };
+
+  let formattedTime = `${time.hours}:${time.minutes}:${time.seconds}`;
+
+  return formattedTime;
+}
+
+const playlist = {
+  getPlaylistDuration: (videoTimeElements) => {
+    let durationSeconds = 0;
+
+    for (let i = 0; i < videoTimeElements.length; i++) {
+      let time = videoTimeElements[i]; // Example: "1:26"
+      durationSeconds += convertTimeToSeconds(time);
+    }
+
+    return convertSecondsToTime(durationSeconds);
+  },
+  getPlaylistAverageDuration: (videoTimeElements) => {
+    let durationSeconds = 0;
+
+    for (let i = 0; i < videoTimeElements.length; i++) {
+      let time = videoTimeElements[i];
+      durationSeconds += convertTimeToSeconds(time);
+    }
+
+    let averageDurationSeconds = videoTimeElements.length > 0
+      ? Math.round(durationSeconds / videoTimeElements.length)
+      : 0;
+
+    return convertSecondsToTime(averageDurationSeconds);
+  },
+  getPlaylistTotalVideos: (videoTimeElements) => {
+    return videoTimeElements.length;
+  },
+  getPlaylistShortestVideo: (videoTimeElements) => {
+    let shortestTime = Infinity;
+
+    for (let i = 0; i < videoTimeElements.length; i++) {
+      let time = videoTimeElements[i];
+      let videoDuration = convertTimeToSeconds(time);
+
+      if (videoDuration < shortestTime) {
+        shortestTime = videoDuration;
+      }
+    }
+
+    return convertSecondsToTime(shortestTime);
+  },
+  getPlaylistLongestVideo: (videoTimeElements) => {
+    let longestTime = 0;
+
+    for (let i = 0; i < videoTimeElements.length; i++) {
+      let time = videoTimeElements[i];
+      let videoDuration = convertTimeToSeconds(time);
+
+      if (videoDuration > longestTime) {
+        longestTime = videoDuration;
+      }
+    }
+
+    return convertSecondsToTime(longestTime);
+  }
+}
